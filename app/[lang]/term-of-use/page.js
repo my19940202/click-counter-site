9a69f218c5732c3404f3884891baab829f07b28f
xishengbo <xishengbo@gmail.com>
2025-01-28 14:52:54 +0800

    add policy and term page

--- app/[lang]/term-of-use/page.js
+++ app/[lang]/term-of-use/page.js
@@ -1,10 +1,10 @@
 import { defaultLocale } from "@/lib/i18n";
-import Content from '@/posts/privacy-policy.mdx'
+import Content from '@/posts/term-of-use.mdx'
 
 export default async function Home({ params }) {
     const langName = params.lang || defaultLocale;
     return (
-        <div className="max-w-[1280px] mx-auto text-lg text-center pb-4">
+        <div className="max-w-[800px] mx-auto text-lg text-left pb-4">
             <Content />
         </div>
     );
