f8cd81510ec38340a96ed5d3f153e6187b77bc55
xishengbo <xishengbo@gmail.com>
2025-02-18 11:54:08 +0800

    刘师傅：优化test接口，便于数据db里面查看

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,9 +1,38 @@
 import { NextResponse } from 'next/server'
+import { supabase } from '@/lib/dbInit';
 
-export async function GET(request) {
+function getFormatedTime() {
+    const now = new Date();
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    };
+    const formatter = new Intl.DateTimeFormat('en-CA', options);
+    return formatter.format(now).replace(',', '');
+}
+
+export async function POST(request) {
+    // 刘师傅装修小程序里面预留的接口 采集最后的订单数据
+    const { bill, user, total } = await request.json();
+    const { error } = await supabase
+        .from('decorations-bill')
+        .insert([{
+            bill, user, total,
+            created_at: getFormatedTime()
+        }]);
+
+    if (error) {
+        return NextResponse.json({ success: false, error: error.message });
+    }
+
     return new NextResponse(JSON.stringify({
         success: true,
-        data: 'this is click counter test api'
+        data: 'add db success'
     }), {
         headers: {
             'Content-Type': 'application/json',
